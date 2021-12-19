956a5d6e4c6046b6ac099f528a77097b2126b5ca
unknown <vickygogeta1@gmail.com>
2021-12-20 00:04:58 +0530

    allowed host fix

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['todoapp-vicky.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['vicky-todo-list.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
